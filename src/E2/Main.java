1b5c62f4b9ce408e3f61fc4132547b75ce876aa5
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 00:20:11 -0500

    Agrega pruebas de contenido en la clase Main
    Se implementan pruebas para la clase genérica ContainerPar,
    mostrando cómo se pueden establecer y obtener los valores
    de las claves y valores. Se imprime el contenido de K y V
    en la consola para verificar su correcto funcionamiento.

--- src/E2/Main.java
+++ src/E2/Main.java
@@ -2,6 +2,16 @@
 
 public class Main {
     public static void main(String[] args) {
+    ContainerPar<String, Integer> par = new ContainerPar<>();
 
+    par.setK("Probando K");
+    String contenidoK = par.getK();
+
+        System.out.println("El Contenido K es: " + contenidoK);
+
+    par.setV(67);
+    Integer contenidoV = par.getV();
+
+        System.out.println("El Contenido V es: " + contenidoV);
     }
 }
