47db066f5845f02d1161149b982ed51671681ff3
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 00:16:19 -0500

    Agrega clase genérica ContainerPar para manejo de pares clave-valor

---
+++ src/E2/ContainerPar.java
@@ -0,0 +1,22 @@
+﻿package E2;
+
+public class ContainerPar <K, V>{
+    private K contenidoK;
+    private V contenidoV;
+
+    public void setK(K contenidoK){
+        this.contenidoK = contenidoK;
+    }
+
+    public void setV(V contenidoV){
+        this.contenidoV = contenidoV;
+    }
+
+    public K getK(){
+        return contenidoK;
+    }
+
+    public V getV(){
+        return contenidoV;
+    }
+}
