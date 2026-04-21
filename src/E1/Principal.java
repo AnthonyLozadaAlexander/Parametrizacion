4d4b5fedfa593f9633ca2a41981727e78d921471
zDefcon <antholozadarequelme@gmail.com>
2026-04-20 23:47:56 -0500

    Agrega método main en la clase Principal para probar la clase Caja
    Se implementa un método main que crea una instancia de la clase genérica Caja,
    almacena un String y recupera su contenido. Esto permite verificar el funcionamiento
    de la clase Caja en el contexto de la aplicación.

--- src/E1/Principal.java
+++ src/E1/Principal.java
@@ -1,4 +1,13 @@
 ﻿package E1;
 
+import E1.Logica.Caja;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        // creo una caja String
+        Caja<String> cajaStr = new Caja<>();
+        cajaStr.ponerAlgo("Probando Generic");
+        String contenido = cajaStr.obtenerAlgo();
+    }
 }
