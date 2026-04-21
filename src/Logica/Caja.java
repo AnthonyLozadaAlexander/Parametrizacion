beec8a8cfc899a3d8aea8e571f990a833efbd73d
zDefcon <antholozadarequelme@gmail.com>
2026-04-20 23:41:48 -0500

    Agrega clase genérica Caja para manejo de contenido

---
+++ src/Logica/Caja.java
@@ -0,0 +1,14 @@
+﻿package Logica;
+// tipo dato generico
+public class Caja <T> {
+
+    private T contenido;
+
+    public void ponerAlgo(T contenido){
+        this.contenido = contenido;
+    }
+
+    public T obtenerAlgo(){
+        return contenido;
+    }
+}
