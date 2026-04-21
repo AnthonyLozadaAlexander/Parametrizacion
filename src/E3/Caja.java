069a4356a4e95ed4de4f55332d048a292122345f
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 00:49:48 -0500

    Renombra método imprimirArr a mostrarArr y mejora su implementación
    Se cambia el nombre del método imprimirArr a mostrarArr para reflejar mejor su funcionalidad.
    Además, se mejora la implementación para utilizar un bucle mejorado y se imprime el índice de cada elemento.

--- src/E3/Caja.java
+++ src/E3/Caja.java
@@ -1,26 +1,28 @@
-﻿package E3;
+package E3;
 
-public class Caja <T>{
+public class Caja<T> {
 
     private T[] data;
 
-    public Caja(T[] data){
+    public Caja(T[] data) {
         this.data = data;
     }
 
-    public void setData(T[] data){
+    public void setData(T[] data) {
         this.data = data;
     }
 
-    public T[] getData(){
+    public T[] getData() {
         return this.data;
     }
 
-    public void imprimirArr(T[] data){
-        for (int i = 0; i <= data.length-1; i++) {
-            if(i == 0) System.out.print("[");
-            System.out.print(data[i] + ", ");
-            if(i == data.length - 1) System.out.println("]");
+    public void mostrarArr(T[] arr) {
+        int count = 0;
+        for (T item : arr) {
+            System.out.println("T[" + count + "]: ");
+            System.out.println(item);
+            count++;
         }
     }
+
 }
