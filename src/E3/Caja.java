d492c38e67009526e0675cae07c915b6f36ea97d
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 00:33:45 -0500

    Agrega método imprimirArr para mostrar contenido de arreglos

--- src/E3/Caja.java
+++ src/E3/Caja.java
@@ -15,4 +15,12 @@ public class Caja <T>{
     public T[] getData(){
         return this.data;
     }
+
+    public void imprimirArr(T[] data){
+        for (int i = 0; i <= data.length-1; i++) {
+            if(i == 0) System.out.print("[");
+            System.out.print(data[i] + ", ");
+            if(i == data.length - 1) System.out.println("]");
+        }
+    }
 }
