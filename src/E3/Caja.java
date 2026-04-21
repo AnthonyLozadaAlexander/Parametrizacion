0e25d0a90be82f0b9d9a77b191c5d754583b4f39
zDefcon <antholozadarequelme@gmail.com>
2026-04-21 00:30:52 -0500

    Agrega clase genérica Caja para manejo de datos

---
+++ src/E3/Caja.java
@@ -0,0 +1,18 @@
+﻿package E3;
+
+public class Caja <T>{
+
+    private T[] data;
+
+    public Caja(T[] data){
+        this.data = data;
+    }
+
+    public void setData(T[] data){
+        this.data = data;
+    }
+
+    public T[] getData(){
+        return this.data;
+    }
+}
